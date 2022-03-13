b82be81db8b151b5f1f23115ef5216fae9d7591e
Hendra <hendra2392@gmail.com>
2022-03-13 15:25:45 +0700

    Exercise

---
+++ chain.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const startingFileName = 'start.txt'
+
+// baca 4 file
+
+// buka file start.txt
+// di dalam file tsb, ada sebuah nama file. buka isi file tsb. // second
+// di dalam file kedua, ada sebuah nama file lg. buka isi file tsb. // third
+// di dalam file ketiga, ada sebuah nama file terakhir. console.log isi file tsb // final
+//
+// pakai fs.readFile
+function roadToFinal(){
+}
+
+roadToFinal()
